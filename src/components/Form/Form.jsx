3073f429f2618132408e9078586f85d95aa27283
Karl Guzman <guzmankarl0410@gmail.com>
2021-02-13 17:33:30 -0800

    some general UI and material UI changes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import {
   InputLabel,
+  Input,
   Button,
   Grid,
   Typography,
   Box,
   TextField,
+  Paper,
+  FormControl,
 } from "@material-ui/core";
+import useStyles from "./styles";
 
 const Form = ({ handleAddTodo }) => {
+  const classes = useStyles();
   const [todo, setTodo] = useState("");
 
   const handleOnClick = (event, todo) => {
@@ -19,25 +24,35 @@ const Form = ({ handleAddTodo }) => {
 
   return (
     <>
-      <Box width="50%" boxShadow={3} margin="auto" p={3}>
-        <Typography variant="h6" gutterBottom>
-          What are you accomplishing today?
-        </Typography>
-        <Grid container spacing={1} mb={3}>
-          <TextField
-            fullWidth
-            margin="normal"
-            name="to do"
-            value={todo}
-            onChange={(event) => setTodo(event.target.value)}
-          />
-          <Button
-            variant="outlined"
-            mt={2}
-            onClick={(event) => handleOnClick(event, todo)}
-          >
-            Add this to your list
-          </Button>
+      <Box width="50%" boxShadow={3} margin="auto" p={5}>
+        <Grid container spacing={5} mb={3}>
+          <Grid item xs={12}>
+            <Typography variant="h6" gutterBottom>
+              What are you accomplishing today?
+            </Typography>
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+            <form>
+              <FormControl fullWidth style={{ paddingBottom: "15px" }}>
+                <InputLabel>Add a todo</InputLabel>
+                <Input
+                  name="to do"
+                  value={todo}
+                  onChange={(event) => setTodo(event.target.value)}
+                />
+              </FormControl>
+              <Button
+                className={classes.button}
+                disabled={!todo}
+                variant="contained"
+                color="primary"
+                onClick={(event) => handleOnClick(event, todo)}
+                type="submit"
+              >
+                Add this task to your day
+              </Button>
+            </form>
+          </Grid>
         </Grid>
       </Box>
     </>
